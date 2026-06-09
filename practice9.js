56f60fb1f71c1cf93dcaaf05488bb61500831b4d
Mohammed Abdul  Muntaqim <muntaqim994@gmail.com>
2026-06-09 08:53:37 +0530

    Add files via upload

---
+++ practice9.js
@@ -0,0 +1,8 @@
+let nm = document.querySelector("#name")
+let form = document.querySelector("form")
+form.addEventListener("submit", function (dets) {
+    dets.preventDefault()
+    if (nm.value.length<=2){
+        document.querySelector("#hide").style.display="initial"
+    }
+})
